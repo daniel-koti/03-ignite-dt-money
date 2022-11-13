e1480742a768151386316513bb26eb39521cff94
daniel-moniz <dkmoniz@cetim.ms>
2022-11-13 21:57:16 +0100

    initial commit 🚀

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,93 @@
+import * as Dialog from '@radix-ui/react-dialog';
+import { ArrowCircleUp, X, ArrowCircleDown } from 'phosphor-react';
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Controller, useForm } from 'react-hook-form'
+
+import { Overlay, Content, CloseButton, TransactionType, TransactionTypeButton } from './styles';
+
+const newTransactionFormSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  type: z.enum(['income', 'outcome'])
+})
+
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
+
+export function NewTransactionModal() {
+  const { 
+    control,
+    register,
+    handleSubmit,
+    formState: {
+      isSubmitting
+    }
+  } = useForm<NewTransactionFormInputs>({
+    resolver: zodResolver(newTransactionFormSchema),
+    defaultValues: {
+      type: 'income',
+    }
+  })
+
+  async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    console.log(data)
+  }
+
+  return (
+    <Dialog.Portal>
+      <Overlay />
+
+      <Content>
+        <Dialog.Title>Nova Transação</Dialog.Title>
+
+        <CloseButton>
+          <X size={24}/>
+        </CloseButton>
+
+        <form action="" onSubmit={handleSubmit(handleCreateNewTransaction)}>
+          <input 
+            type="text" 
+            placeholder='Descrição' 
+            {...register('description', { required: 'Campo obrigatório'})} />
+          <input 
+            type="number"
+            placeholder='Preço' required
+            {...register('price', {
+              valueAsNumber: true,
+            })}
+          />
+          <input 
+            type="text"
+            placeholder='Categoria' required
+            {...register('category')}
+          />
+
+          <Controller 
+            control={control} 
+            name='type' 
+            render={({ field }) => {
+              return (
+                <TransactionType onValueChange={field.onChange} value={field.value}>
+                  <TransactionTypeButton variant='income' value='income'>
+                    <ArrowCircleUp size={24} />
+                    Entrada
+                  </TransactionTypeButton>
+
+                  <TransactionTypeButton variant='outcome' value='outcome'>
+                    <ArrowCircleDown size={24} />
+                    Saída
+                  </TransactionTypeButton>
+                </TransactionType>
+              )
+            }} />
+
+          <button type='submit' disabled={isSubmitting}>
+            Cadastrar
+          </button>
+        </form>
+      </Content>
+    </Dialog.Portal>
+  )
+}
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,41 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { useTheme } from "styled-components";
+import { SummaryCard, SummaryContainer } from "./styles";
+import { priceFormatter } from '../../utils/formatter';
+import { useSummary } from "../../hooks/useSummary";
+
+export function Summary() {
+  const colors = useTheme()
+  const summary = useSummary()
+
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color={colors["green-300"]}/>
+        </header>
+
+        <strong>{priceFormatter.format(summary.income)}</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color={colors["red-300"]}/>
+        </header>
+
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Entradas</span>
+          <CurrencyDollar size={32} color={colors.white}/>
+        </header>
+
+        <strong>{priceFormatter.format(summary.total)}</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
+}
